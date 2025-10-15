5ae7b4cd36853b3b82fcd1b0db57a4a1d312e952
Luis Perez <luis.perez.3@asu.edu>
2025-10-15 02:59:06 +0000

    Refactor movie selection logic to populate movie list based on genre selection

--- script.js
+++ script.js
@@ -3,17 +3,36 @@ document.getElementById("genre").addEventListener("change", function() {
   // Get the selected genre
   const selectedGenre = document.getElementById("genre").value;
 
+  // Create a variable to hold the movie list
+  let movieList = [];
+
   // Add movies to the movieList based on the selected genre
   if (selectedGenre === "Comedy") {
-    console.log("You selected 'Comedy'");
+    // Add comedy movies directly to movieList
+    movieList.push("The Mask");
+    movieList.push("Superbad");
+    movieList.push("Home Alone");
+    movieList.push("Dumb and Dumber");
   } else if (selectedGenre === "Action") {
-    console.log("You selected 'Action'");
+    // Add action movies directly to movieList
+    movieList.push("Die Hard");
+    movieList.push("Mad Max: Fury Road");
+    movieList.push("John Wick");
+    movieList.push("The Avengers");
   } else if (selectedGenre === "Drama") {
-    console.log("You selected 'Drama'");
+    // Add drama movies directly to movieList
+    movieList.push("Forrest Gump");
+    movieList.push("The Shawshank Redemption");
+    movieList.push("A Beautiful Mind");
+    movieList.push("The Pursuit of Happyness");
   } else if (selectedGenre === "Sci-Fi") {
-    console.log("You selected 'Sci-Fi'");
+    // Add sci-fi movies directly to movieList
+    movieList.push("Interstellar");
+    movieList.push("Inception");
+    movieList.push("The Matrix");
+    movieList.push("Star Wars: A New Hope");
   }
 
   // Display the list of movies on the page
-  document.getElementById("movieRecommendations").innerText = `Enjoy: !`;
+  document.getElementById("movieRecommendations").innerText = `Enjoy: ${movieList.join(", ")}`;
 });
